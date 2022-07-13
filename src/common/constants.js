4bb64fc512b3333f8377a41bc00cea9cd33cb91e
junyu33 <2658799217@qq.com>
2022-07-13 10:09:27 +0800

    restore remote oj

--- src/common/constants.js
+++ src/common/constants.js
@@ -154,6 +154,30 @@ export const PROBLEM_LEVEL={
 
 
 export const REMOTE_OJ = [
+  // {
+  //   name:'HDU',
+  //   key:"HDU"
+  // },
+  // {
+  //   name:"Codeforces",
+  //   key:"CF"
+  // },
+  // {
+  //   name:"POJ",
+  //   key:"POJ"
+  // },
+  // {
+  //   name:"GYM",
+  //   key:"GYM"
+  // },
+  // {
+  //   name:"AtCoder",
+  //   key:"AC"
+  // },
+  // {
+  //   name:"SPOJ",
+  //   key:"SPOJ"
+  // }
 
 
 ]
