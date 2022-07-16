59fa7b0de2a2a55a8f8785173a8a9d7926bdd93f
junyu33 <2658799217@qq.com>
2022-07-16 17:35:35 +0800

    fixed a bug

--- src/common/api.js
+++ src/common/api.js
@@ -120,14 +120,8 @@ axios.interceptors.response.use(
 const ojApi = {
   // Home页的请求
   getMotto(){
-    fetch('https://v1.hitokoto.cn')
-        .then(response => response.json())
-        .then(data => {
-          const hitokoto = document.getElementById('hitokoto_text')
-          hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-          hitokoto.innerText = data.hitokoto
-        })
-        .catch(console.error)
+    return ajax('https://api.fghrsh.net/hitokoto/rand/?encode=jsc&uid=3335', 'get', {
+    })
   },
   getWebsiteConfig(){
     return ajax('/api/get-website-config', 'get', {
