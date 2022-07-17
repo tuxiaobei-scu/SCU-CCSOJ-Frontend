cada2fefb3c336db5f370138c30f9efa97f5f3e8
ACyanA <2316769691@qq.com>
2022-07-17 15:22:29 +0800

    rp history相关修改

--- src/views/oj/RPstatus/RPChangeList.vue
+++ src/views/oj/RPstatus/RPChangeList.vue
@@ -9,24 +9,13 @@
                   $t('m.Status')
                 }}</span>
             </el-col>
-            <el-col :xs="10" :sm="8" :md="4" :lg="4">
-              <el-switch
-                  style="display: block"
-                  v-model="formFilter.onlyMine"
-                  :active-text="$t('m.Mine')"
-                  :width="40"
-                  @change="handleOnlyMine"
-                  :inactive-text="$t('m.All')"
-              >
-              </el-switch>
-            </el-col>
             <el-col :sm="8" :md="5" :lg="4" class="hidden-xs-only">
               <el-button
                   type="primary"
                   size="small"
                   icon="el-icon-refresh"
                   round
-                  @click="getRPChange"
+                  @click="getRpchanges"
               >{{ $t('m.Refresh') }}</el-button
               >
             </el-col>
@@ -36,30 +25,8 @@
                   size="small"
                   icon="el-icon-refresh"
                   circle
-                  @click="getRPChange"
+                  @click="getRpchanges"
               ></el-button>
-            </el-col>
-
-            <el-col :xs="24" :sm="12" :md="5" :lg="5" class="search">
-              <vxe-input
-                  v-model="formFilter.problemID"
-                  :placeholder="$t('m.Enter_Problem_ID')"
-                  type="search"
-                  size="medium"
-                  @keyup.enter.native="handleQueryChange('probemID')"
-                  @search-click="handleQueryChange('probemID')"
-              ></vxe-input>
-            </el-col>
-            <el-col :xs="24" :sm="12" :md="5" :lg="5" class="search">
-              <vxe-input
-                  v-model="formFilter.username"
-                  :disabled="formFilter.onlyMine"
-                  :placeholder="$t('m.Enter_Author')"
-                  type="search"
-                  size="medium"
-                  @keyup.enter.native="handleQueryChange('username')"
-                  @search-click="handleQueryChange('username')"
-              ></vxe-input>
             </el-col>
           </el-row>
         </div>
@@ -73,20 +40,27 @@
             align="center"
             auto-resize
             :row-class-name="tableRowClassName"
-            :data="RpChanges"
+            :data="rpchanges"
             :loading="loadingTable"
         >
           <vxe-table-column
               field="RPChangeID"
               :title="$t('m.RPChange_ID')"
               width="100"
-          ></vxe-table-column>
+          >
+            <template v-slot="{ row }">
+              <span>{{ row.rpchangeId }}</span>
+            </template>
+          </vxe-table-column>
 
           <vxe-table-column
               field="RPChange"
               :title="$t('m.RPChange')"
               min-width="80"
           >
+            <template v-slot="{ row }">
+              <span>{{ row.rpchange }}</span>
+            </template>
           </vxe-table-column>
 
           <vxe-table-column
@@ -108,12 +82,25 @@
               :title="$t('m.RPChange_Time')"
               min-width="96"
           >
+            <template v-slot="{ row }">
+              <span
+              ><el-tooltip
+                  :content="row.rpchangeTime | localtime"
+                  placement="top"
+              >
+                  <span>{{ row.rpchangeTime | fromNow }}</span>
+                </el-tooltip></span
+              >
+            </template>
           </vxe-table-column>
           <vxe-table-column
               field="Description"
               :title="$t('m.Description')"
               min-width="80"
           >
+            <template v-slot="{ row }">
+              <span>{{ row.description }}</span>
+            </template>
           </vxe-table-column>
         </vxe-table>
       </el-card>
@@ -132,12 +119,18 @@
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import api from '@/common/api';
+import {
+  JUDGE_STATUS,
+  CONTEST_STATUS,
+  JUDGE_STATUS_RESERVE,
+  RULE_TYPE,
+} from '@/common/constants';
 import utils from '@/common/utils';
 import Pagination from '@/components/oj/common/Pagination';
 import myMessage from '@/common/message';
 import 'element-ui/lib/theme-chalk/display.css';
 export default {
-  name: 'RPChangeList',
+  name: 'rpchangeList',
   components: {
     Pagination,
   },
@@ -150,7 +143,7 @@ export default {
         problemID: '',
       },
       loadingTable: false,
-      RPchanges: [],
+      rpchanges: [],
       needCheckSubmitIds: {}, // 当前状态为6和7的提交记录Id 需要重新检查更新
       total: 30,
       limit: 15,
@@ -172,9 +165,16 @@ export default {
   },
   mounted() {
     this.JUDGE_STATUS = Object.assign({}, JUDGE_STATUS);
+    this.JUDGE_STATUS_LIST = Object.assign({}, JUDGE_STATUS);
     this.JUDGE_STATUS_RESERVE = Object.assign({}, JUDGE_STATUS_RESERVE);
     this.CONTEST_STATUS = Object.assign({}, CONTEST_STATUS);
     this.RULE_TYPE = Object.assign({}, RULE_TYPE);
+    // 去除下拉框选择中的Compiling,Judging,Submitting,Not Submitted,Submitted Unknown Result 五种状态
+    delete this.JUDGE_STATUS_LIST['6'];
+    delete this.JUDGE_STATUS_LIST['7'];
+    delete this.JUDGE_STATUS_LIST['9'];
+    delete this.JUDGE_STATUS_LIST['-5'];
+    delete this.JUDGE_STATUS_LIST['-10'];
     this.getData();
   },
   methods: {
@@ -201,7 +201,7 @@ export default {
     },
 
     getData() {
-      this.getSubmissions();
+      this.getRpchanges();
     },
 
     buildQuery() {
@@ -216,8 +216,17 @@ export default {
       };
     },
 
-    getRPChange() {
+    getRpchanges() {
       let params = this.buildQuery();
+      params.contestID = this.contestID;
+      params.gid = this.groupID;
+      if (this.contestID) {
+        if (this.contestStatus == CONTEST_STATUS.SCHEDULED) {
+          params.beforeContestSubmit = true;
+        } else {
+          params.beforeContestSubmit = false;
+        }
+      }
       if (this.formFilter.onlyMine) {
         // 需要判断是否为登陆状态
         if (this.isAuthenticated) {
@@ -229,21 +238,105 @@ export default {
           return;
         }
       }
+
       this.loadingTable = true;
-      this.submissions = [];
+      this.rpchanges = [];
       this.needCheckSubmitIds = {};
       let func = 'getRPChangeList';
       api[func](this.limit, utils.filterEmptyValue(params))
           .then((res) => {
             let data = res.data.data;
             let index = 0;
+            for (let v of data.records) {
+              if (
+                  v.status == JUDGE_STATUS_RESERVE['Pending'] ||
+                  v.status == JUDGE_STATUS_RESERVE['Compiling'] ||
+                  v.status == JUDGE_STATUS_RESERVE['Judging']
+              ) {
+                this.needCheckSubmitIds[v.submitId] = index;
+              }
+              v.loading = false;
+              v.index = index;
+              index += 1;
+            }
             this.loadingTable = false;
-            this.submissions = data.records;
+            this.rpchanges = data.records;
             this.total = data.total;
+            if (Object.keys(this.needCheckSubmitIds).length > 0) {
+              this.checkRpchangesStatus();
+            }
           })
           .catch(() => {
             this.loadingTable = false;
           });
+    },
+    // 对当前提交列表 状态为Pending（6）和Judging（7）的提交记录每2秒查询一下最新结果
+    checkRpchangesStatus() {
+      // 使用setTimeout避免一些问题
+      if (this.refreshStatus) {
+        // 如果之前的提交状态检查还没有停止,则停止,否则将会失去timeout的引用造成无限请求
+        clearTimeout(this.refreshStatus);
+        this.autoCheckOpen = false;
+      }
+      const checkStatus = () => {
+        let submitIds = this.needCheckSubmitIds;
+        let func = this.contestID
+            ? 'checkContestRpchangesStatus'
+            : 'checkRpchangesStatus';
+        api[func](Object.keys(submitIds), this.contestID).then(
+            (res) => {
+              let result = res.data.data;
+              if (!this.$refs.xTable) {
+                // 避免请求一半退出view保错
+                return;
+              }
+              let viewData = this.$refs.xTable.getTableData().tableData;
+              for (let key in submitIds) {
+                let submitId = parseInt(key);
+                if (!result[submitId]) {
+                  continue;
+                }
+                // 更新数据列表
+                this.rpchanges[submitIds[key]] = result[submitId];
+                // 更新view中的结果，f分数，耗时，空间消耗，判题机ip
+                viewData[submitIds[key]].status = result[submitId].status;
+                viewData[submitIds[key]].score = result[submitId].score;
+                viewData[submitIds[key]].time = result[submitId].time;
+                viewData[submitIds[key]].memory = result[submitId].memory;
+                viewData[submitIds[key]].judger = result[submitId].judger;
+                // 重新加载这行数据到view中
+                this.$refs.xTable.reloadRow(viewData[submitIds[key]], null, null);
+
+                if (
+                    result[submitId].status != JUDGE_STATUS_RESERVE['Pending'] &&
+                    result[submitId].status != JUDGE_STATUS_RESERVE['Compiling'] &&
+                    result[submitId].status != JUDGE_STATUS_RESERVE['Judging']
+                ) {
+                  delete this.needCheckSubmitIds[key];
+                }
+              }
+              // 当前提交列表的提交都判题结束或者检查结果600s（2s*300）还没判题结束，为了避免无用请求加重服务器负担，直接停止检查的请求。
+              if (
+                  Object.keys(this.needCheckSubmitIds).length == 0 ||
+                  this.checkStatusNum == 300
+              ) {
+                clearTimeout(this.refreshStatus);
+                this.autoCheckOpen = false;
+              } else {
+                this.checkStatusNum += 1;
+                this.refreshStatus = setTimeout(checkStatus, 2000);
+              }
+            },
+            (res) => {
+              clearTimeout(this.refreshStatus);
+              this.autoCheckOpen = false;
+            }
+        );
+      };
+      // 设置每2秒检查一下提交结果
+      this.checkStatusNum += 1;
+      this.refreshStatus = setTimeout(checkStatus, 2000);
+      this.autoCheckOpen = true;
     },
     onPageSizeChange(pageSize) {
       this.limit = pageSize;
@@ -271,6 +364,19 @@ export default {
           equal = false;
         }
       }
+
+      if (!equal) {
+        // 避免重复同个路径请求导致报错
+        let routeName = queryParams.contestID
+            ? 'ContestRpchangeList'
+            : this.groupID
+                ? 'GroupRpchangeList'
+                : 'RpchangeList';
+        this.$router.push({
+          name: routeName,
+          query: queryParams,
+        });
+      }
     },
     goRoute(route) {
       this.$router.push(route);
@@ -297,47 +403,7 @@ export default {
       this.currentPage = 1;
       this.changeRoute();
     },
-    handleOnlyMine() {
-      if (this.formFilter.onlyMine) {
-        // 需要判断是否为登陆状态
-        if (this.isAuthenticated) {
-          this.formFilter.username = '';
-        } else {
-          this.formFilter.onlyMine = false;
-          myMessage.error(this.$i18n.t('m.Please_login_first'));
-          return;
-        }
-      }
-      this.currentPage = 1;
-      this.changeRoute();
-    },
-    ...mapActions(['changeModalStatus']),
-    getProblemUri(pid) {
-      if (this.contestID) {
-        this.$router.push({
-          name: 'ContestProblemDetails',
-          params: {
-            contestID: this.$route.params.contestID,
-            problemID: pid,
-          },
-        });
-      } else if (this.groupID) {
-        this.$router.push({
-          name: 'GroupProblemDetails',
-          params: {
-            problemID: pid,
-            groupID: this.groupID,
-          },
-        });
-      } else {
-        this.$router.push({
-          name: 'ProblemDetails',
-          params: {
-            problemID: pid,
-          },
-        });
-      }
-    },
+
 
     tableRowClassName({ row, rowIndex }) {
       if (row.username == this.userInfo.username && this.isAuthenticated) {
@@ -359,17 +425,17 @@ export default {
       if (!this.contestID) {
         return 'Status';
       } else if (this.problemID) {
-        return 'Problem Submissions';
+        return 'Problem Rpchanges';
       } else {
-        return 'Submissions';
+        return 'Rpchanges';
       }
     },
     status() {
       return this.formFilter.status === ''
-          ? this.$i18n.t('m.RPStatus')
+          ? this.$i18n.t('m.Status')
           : JUDGE_STATUS[this.formFilter.status]
               ? JUDGE_STATUS[this.formFilter.status].name
-              : this.$i18n.t('m.RPStatus');
+              : this.$i18n.t('m.Status');
     },
     rejudgeColumnVisible() {
       return this.isSuperAdmin;
